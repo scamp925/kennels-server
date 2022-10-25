d93c2946c9ab1aafd646a76cda8166ab21682ed9
Sariah Campopiano <scampopiano925@gmail.com>
2022-10-25 14:54:24 -0500

    Animal's response is now embedded with a customer

--- views/animal_requests.py
+++ views/animal_requests.py
@@ -1,6 +1,6 @@
 import sqlite3
 import json
-from models import Animal, Location
+from models import Animal, Location, Customer
 
 ANIMALS = [
     {
@@ -53,10 +53,14 @@ def get_all_animals():
             a.location_id,
             a.customer_id,
             l.name location_name,
-            l.address location_address
+            l.address location_address,
+            c.name customer_name,
+            c.address customer_address
         FROM Animal a
         JOIN Location l
             ON l.id = a.location_id
+        JOIN Customer c
+            ON c.id = a.customer_id
         """)
 
         # Initialize an empty list to hold all animal representations
@@ -74,9 +78,14 @@ def get_all_animals():
 
             # Create a Location instance from the current row
             location = Location(row['id'], row['location_name'], row['location_address'])
+            
+            # Create a Location instance from the current row
+            customer = Customer(row['id'], row['customer_name'], row['customer_address'])
 
             # Add the dictionary representation of the location to the animal
             animal.location = location.__dict__
+            
+            animal.customer = customer.__dict__
 
             # Add the dictionary representation of the animal to the list
             animals.append(animal.__dict__)
